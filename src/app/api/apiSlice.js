fe89183e5f1ee552dfb4af2842b414611b03dc76
Glthub-owner <iliajaghmaidze@gmail.com>
2023-04-04 22:20:23 +0400

    Updated baseURL

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-ilia.onrender.com",
+  baseUrl: "https://technotes-api-xrg4.onrender.com/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
